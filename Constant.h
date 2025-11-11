f6c6e5e84fbd99bb12ff182cdf15c45f70eb97b3
heimdallr <heimdalr@ngs.ru>
2025-11-11 13:17:13 +0700

    inpx constant

--- Constant.h
+++ Constant.h
@@ -8,3 +8,20 @@ constexpr auto IMAGES   = "images";
 constexpr auto PICTURES = "pictures";
 
 }
+
+namespace HomeCompa::Inpx
+{
+constexpr auto STRUCTURE_INFO                     = "structure.info";
+constexpr auto VERSION_INFO                       = L"version.info";
+constexpr auto COLLECTION_INFO                    = L"collection.info";
+constexpr auto REVIEWS_FOLDER                     = L"reviews";
+constexpr auto REVIEWS_ADDITIONAL_ARCHIVE_NAME    = L"additional.zip";
+constexpr auto REVIEWS_ADDITIONAL_BOOKS_FILE_NAME = "books.json";
+constexpr auto COMPILATIONS                       = L"compilations.7z";
+constexpr auto COMPILATIONS_JSON                  = "compilations.json";
+constexpr auto AUTHORS_FOLDER                     = L"authors";
+
+constexpr auto TIME                               = "time";
+constexpr auto NAME                               = "name";
+constexpr auto TEXT                               = "text";
+}
