84907c7cbbd8c84ca5994ea7bf9fe6de973b5966
heimdallr <heimdalrnsk@gmail.com>
2026-02-13 13:47:53 +0700

    Класс сохранения/восстановления лэйаута окна уехал в общее место

---
+++ util/GeometryRestorable.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "fnd/NonCopyMovable.h"
+#include "fnd/memory.h"
+
+#include "util/ISettings.h"
+
+#include "export/util.h"
+
+class QSplitter;
+
+namespace HomeCompa::Util
+{
+
+class UTIL_EXPORT GeometryRestorable
+{
+	NON_COPY_MOVABLE(GeometryRestorable)
+
+public:
+	class IObserver // NOLINT(cppcoreguidelines-special-member-functions)
+	{
+	public:
+		virtual ~IObserver()                  = default;
+		virtual QWidget& GetWidget() noexcept = 0;
+
+		virtual void OnFontChanged(const QFont&)
+		{
+		}
+	};
+
+protected:
+	GeometryRestorable(IObserver& observer, std::shared_ptr<ISettings> settings, QString name);
+	~GeometryRestorable();
+
+protected:
+	void LoadGeometry();
+	void SaveGeometry();
+
+private:
+	class Impl;
+	PropagateConstPtr<Impl> m_impl;
+};
+
+class UTIL_EXPORT GeometryRestorableObserver : virtual public GeometryRestorable::IObserver
+{
+protected:
+	explicit GeometryRestorableObserver(QWidget& widget);
+
+protected: // GeometryRestorable::IObserver
+	QWidget& GetWidget() noexcept override;
+
+private:
+	QWidget& m_widget;
+};
+
+} // namespace HomeCompa::Util
--- util/util.cmake
+++ util/util.cmake
@@ -5,6 +5,7 @@ AddTarget(util	shared_lib
 	LINK_LIBRARIES
 		Qt${QT_MAJOR_VERSION}::Core
 		Qt${QT_MAJOR_VERSION}::Gui
+		Qt${QT_MAJOR_VERSION}::Widgets
 		XercesC::XercesC
 		cimg::cimg
 	LINK_TARGETS
