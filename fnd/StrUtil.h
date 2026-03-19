7051003d73f144e4025031b28f476930581842dd
heimdallr <heimdalrnsk@gmail.com>
2026-03-19 23:51:48 +0700

    #1117 До util из zip никак - рекурсия
    - Новая либа platform, сюда будем класть платформозависимые штуки

---
+++ fnd/StrUtil.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <format>
+
+#include <QString>
+
+template <>
+struct std::formatter<QString> : std::formatter<std::string>
+{
+	auto format(const QString& obj, std::format_context& ctx) const
+	{
+		return std::formatter<std::string>::format(obj.toStdString(), ctx);
+	}
+};
+
+template <>
+struct std::formatter<std::filesystem::path> : std::formatter<std::string>
+{
+	auto format(const std::filesystem::path& obj, std::format_context& ctx) const
+	{
+		return std::formatter<std::string>::format(obj.string(), ctx);
+	}
+};
--- fnd/fnd.cmake
+++ fnd/fnd.cmake
@@ -1,5 +1,7 @@
-AddTarget(fnd	header_only
+AddTarget(fnd	shared_lib
 	PROJECT_GROUP Foundation
 	SOURCE_DIRECTORY
 		"${CMAKE_CURRENT_LIST_DIR}"
+	LINK_LIBRARIES
+		Qt${QT_MAJOR_VERSION}::Core
 )
