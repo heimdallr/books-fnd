abd8f57f4edc46622d2d73b0f8988eecb7ecc12d
heimdallr <heimdalrnsk@gmail.com>
2026-04-13 15:22:51 +0700

    #1179 Костыль для разделения версий Qt

---
+++ QtTypes.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <ranges>
+#include <set>
+#include <unordered_set>
+
+#include <QString>
+#include <QVariant>
+#include <QtGlobal>
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+
+using qsizetype_t = qsizetype;
+using qintptr_t   = qintptr;
+
+template <typename T>
+inline void RemoveIf(QString& value, T&& functor)
+{
+	value.removeIf(std::forward<T>(functor));
+}
+
+inline QString First(const QString& string, const qsizetype n)
+{
+	return string.first(n);
+}
+
+inline QString Last(const QString& string, const qsizetype n)
+{
+	return string.last(n);
+}
+
+template <typename T>
+inline void Erase(QString& string, const T& begin, const T& end)
+{
+	string.erase(begin, end);
+}
+
+template <typename T>
+inline void Erase(QString& string, const T& it)
+{
+	string.erase(it);
+}
+
+inline int TypeId(const QVariant& var)
+{
+	return var.typeId();
+}
+
+	#define BEGIN_FILTER_CHANGE beginFilterChange()
+
+	#define END_FILTER_CHANGE endFilterChange(Direction::Rows)
+
+#elif QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+
+using qsizetype_t = int;
+using qintptr_t   = long;
+
+template <typename T>
+inline void RemoveIf(QString& value, T&& functor)
+{
+	std::ranges::transform(value, value.begin(), std::forward<T>(functor));
+}
+
+inline QString First(const QString& string, const qsizetype n)
+{
+	return string.mid(0, n);
+}
+
+inline QString Last(const QString& string, const qsizetype n)
+{
+	return string.mid(string.length() - n);
+}
+
+template <typename T>
+inline void Erase(QString& string, const T& begin, const T& end)
+{
+	string.mid(0, begin - string.begin()) + string.mid(end - string.begin(), string.length());
+}
+
+template <typename T>
+inline void Erase(QString& string, const T& it)
+{
+	Erase(string, it, std::next(it));
+}
+
+inline int TypeId(const QVariant& var)
+{
+	return var.type();
+}
+
+	#define BEGIN_FILTER_CHANGE (void)0
+
+	#define END_FILTER_CHANGE invalidateFilter()
+
+Q_DECLARE_METATYPE(std::set<QString>)
+Q_DECLARE_METATYPE(std::unordered_set<QString>)
+
+#else
+	#error unsupported qt version
+#endif
