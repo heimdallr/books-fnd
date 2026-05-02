40bd6b3d2e0e779cf03c11af5fd11aa06e333bd8
heimdallr <heimdalrnsk@gmail.com>
2026-05-02 16:12:38 +0700

    Пул потоков обучен контексту. Нраица

--- util/executor/ThreadPool.h
+++ util/executor/ThreadPool.h
@@ -10,26 +10,34 @@
 namespace HomeCompa::Util
 {
 
+template <typename T = size_t>
 class ThreadPool
 {
 public:
 	struct Initializer
 	{
-		unsigned int threadCount { std::thread::hardware_concurrency() };
-		size_t       maxQueueSize { std::numeric_limits<size_t>::max() };
+		unsigned int             threadCount { std::thread::hardware_concurrency() };
+		size_t                   maxQueueSize { std::numeric_limits<size_t>::max() };
+		std::function<T(size_t)> contextGetter { [](size_t) {
+			return T {};
+		} };
 	};
 
 public:
 	explicit ThreadPool(const Initializer& initializer = {})
 		: m_maxQueueSize { initializer.maxQueueSize }
 	{
+		m_contexts.reserve(initializer.threadCount);
+		std::ranges::transform(std::views::iota(decltype(initializer.threadCount) { 0 }, initializer.threadCount), std::back_inserter(m_contexts), [&](const auto n) {
+			return initializer.contextGetter(n);
+		});
 		m_threads.reserve(initializer.threadCount);
-		std::ranges::transform(std::views::iota(decltype(initializer.threadCount) { 0 }, initializer.threadCount), std::back_inserter(m_threads), [this](auto) {
-			return std::jthread(std::bind_front(&ThreadPool::work, this));
+		std::ranges::transform(std::views::iota(decltype(initializer.threadCount) { 0 }, initializer.threadCount), std::back_inserter(m_threads), [&](const auto n) {
+			return std::jthread(std::bind_front(&ThreadPool::work, this, std::ref(m_contexts[n])));
 		});
 	}
 
-	void enqueue(std::function<void()> task)
+	void enqueue(std::function<void(T&)> task)
 	{
 		{
 			std::unique_lock lock(m_tasksGuard);
@@ -42,7 +50,7 @@ public:
 		m_condition.notify_one();
 	}
 
-	void wait()
+	std::vector<T> wait()
 	{
 		{
 			std::unique_lock lock(m_tasksGuard);
@@ -52,10 +60,13 @@ public:
 		}
 
 		m_threads.clear();
+
+		auto contexts = std::move(m_contexts);
+		return contexts;
 	}
 
 private:
-	std::function<void()> getTask(const std::stop_token& stop)
+	std::function<void(T&)> getTask(const std::stop_token& stop)
 	{
 		std::unique_lock lock(m_tasksGuard);
 		if (!m_condition.wait(lock, stop, [this] {
@@ -70,19 +81,20 @@ private:
 		return task;
 	}
 
-	void work(const std::stop_token stop)
+	void work(T& t, const std::stop_token stop)
 	{
 		while (!stop.stop_requested())
 			if (const auto task = getTask(stop))
-				task();
+				task(t);
 	}
 
 private:
-	const size_t                      m_maxQueueSize;
-	std::queue<std::function<void()>> m_tasks;
-	std::mutex                        m_tasksGuard;
-	std::condition_variable_any       m_condition;
-	std::vector<std::jthread>         m_threads;
+	const size_t                        m_maxQueueSize;
+	std::queue<std::function<void(T&)>> m_tasks;
+	std::mutex                          m_tasksGuard;
+	std::condition_variable_any         m_condition;
+	std::vector<T>                      m_contexts;
+	std::vector<std::jthread>           m_threads;
 };
 
 } // namespace HomeCompa::Util
