fc32f42202e4d23c64e071936dbd702d55b0a8b5
heimdallr <heimdalrnsk@gmail.com>
2026-05-05 15:55:55 +0700

    Мелкий переезд в дереве проекта

--- zip/zip.cmake
+++ zip/zip.cmake
@@ -1,7 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 AddTarget(zip	shared_lib
-	PROJECT_GROUP Foundation/zip
+	PROJECT_GROUP Foundation
 	SOURCE_DIRECTORY
 		"${CMAKE_CURRENT_LIST_DIR}"
     INCLUDE_DIRECTORIES
