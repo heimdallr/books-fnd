5888eaa9cc5a9e6db0f6c090383479969eb19a16
Alexander Chiryatiev <heimdallrnsk@gmail.com>
2026-05-11 17:49:59 +0700

    Не надо линковать эту либу. Она грузится динамически

--- djvu/djvu.cmake
+++ djvu/djvu.cmake
@@ -1,8 +1,8 @@
 AddTarget(flidjvu		shared_lib
 	PROJECT_GROUP		Foundation
 	SOURCE_DIRECTORY	"${CMAKE_CURRENT_LIST_DIR}"
+	INCLUDE_DIRECTORIES     "${djvulibre_INCLUDE_DIR}"
 	LINK_LIBRARIES
-		djvulibre::djvulibre
 		Qt${QT_MAJOR_VERSION}::Core
 		Qt${QT_MAJOR_VERSION}::Gui
 	LINK_TARGETS
