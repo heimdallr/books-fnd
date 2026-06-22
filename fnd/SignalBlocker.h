45ecd2ebef88476499277b38d327f5a25670add2
heimdallr <heimdalrnsk@gmail.com>
2026-06-22 13:31:16 +0700

    SignalBlocker

---
+++ fnd/SignalBlocker.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QtCore/QSignalBlocker>
+
+template <typename T>
+class SignalBlocker
+{
+public:
+	explicit SignalBlocker(T *object)
+		: object_(object)
+		, blocker_(object)
+	{
+	}
+
+	T *operator->() { return object_; }
+
+private:
+	T *object_;
+	QSignalBlocker blocker_;
+};
