27472f53a86a1d3f68ffaffb94e18b600212e32c
heimdallr <heimdalrnsk@gmail.com>
2026-09-04 00:43:18 +0700

    Хватит уже рубиться за файл лога. Не дают писать в этот - пишем в соседний.

--- logging/init.cpp
+++ logging/init.cpp
@@ -1,6 +1,9 @@
 #include "init.h"
 
 #include <QCommandLineParser>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
 
 #include <plog/Appenders/RollingFileAppender.h>
 #include <plog/Formatters/TxtFormatter.h>
@@ -11,6 +14,24 @@
 
 using namespace HomeCompa::Log;
 
+namespace
+{
+
+std::string CheckForAppend(QString path)
+{
+	const QFileInfo fileInfo(path);
+	for (int i = 0;; path = fileInfo.dir().filePath(QString("%1_%2.%3").arg(fileInfo.completeBaseName()).arg(++i).arg(fileInfo.suffix())))
+	{
+		QFile file(path);
+		if (file.open(QIODevice::WriteOnly | QIODevice::Append))
+			break;
+	}
+
+	return path.toStdString();
+}
+
+}
+
 struct LoggingInitializer::Impl
 {
 	plog::RollingFileAppender<plog::TxtFormatter> rollingFileAppender;
@@ -18,7 +39,7 @@ struct LoggingInitializer::Impl
 	QtLogHandler                                  qtLogHandler;
 
 	explicit Impl(const QString& path)
-		: rollingFileAppender(path.toStdString().data(), 1024ULL * 1024 * 1024, 10)
+		: rollingFileAppender(CheckForAppend(path).data(), 1024ULL * 1024 * 1024, 10)
 		, logAppender(&rollingFileAppender)
 	{
 	}
